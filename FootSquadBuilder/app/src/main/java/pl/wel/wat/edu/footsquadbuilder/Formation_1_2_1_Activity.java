565f6ed597a8a0ddb093b95b0fbb6c331ff9eb5b
Kacper Janowski <kacper.janowski@student.wat.edu.pl>
2021-06-17 23:43:47 +0200

    Formacja 1 2 1

---
+++ FootSquadBuilder/app/src/main/java/pl/wel/wat/edu/footsquadbuilder/Formation_1_2_1_Activity.java
@@ -0,0 +1,14 @@
+package pl.wel.wat.edu.footsquadbuilder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Formation_1_2_1_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formation121);
+    }
+}
